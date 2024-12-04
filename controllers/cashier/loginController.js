69da2d4f40751b91ae3576dcc251a16a555a8759
usman <usman.hasan619@gmail.com>
2024-12-04 17:03:33 +0530

    fix session api

--- controllers/cashier/loginController.js
+++ controllers/cashier/loginController.js
@@ -14,32 +14,45 @@ const Session = require("../../models/session/sessionModel");
 exports.loginUser = asyncHandler(async (req, res) => {
   const { code } = req.body;
 
+  // Generate a unique session ID using uuid
+  const session = uuidv4();
+
   // Validate that the code is provided
-  if (!code) {
-    return res.status(400).json({ message: "Code is required" });
-  }
+  if (!code) return res.status(400).json({ message: "Code is required" });
 
   // Find the user by the provided code
   const user = await User.findOne({ code });
 
-  if (!user) {
+  if (!user)
     return res.status(400).json({ success: false, message: "Invalid code" });
-  }
-
-  // Generate a unique session ID using uuid
-  const session = uuidv4();
 
   // session Data Mongodb Database
-  const newSession = new Session({
-    session,
-    userId: user._id,
-  });
-  // Save the session to the database
-  await newSession.save();
+  const findSessionByUserId = await Session?.findOne({ userId: user?._id });
+
+  if (!findSessionByUserId) {
+    // Save the session to the database
+    await Session.create({
+      session,
+      userId: user._id,
+    });
+  }
+
+  // update session Mongodb Database
+  if (findSessionByUserId?.session) {
+    await Session?.findOneAndUpdate(
+      { session: findSessionByUserId?.session },
+      {
+        $set: {
+          session,
+        },
+      },
+      { new: true }
+    );
+  }
 
   // Generate and send JWT token
   const token = jwt.sign(
-    { user: { username: user.fullname, email: user.email, id: user._id } },
+    { user: { username: user?.fullname, email: user?.email, id: user?._id } },
     process.env.JWT_SECRET,
     { expiresIn: "30 day" }
   );
@@ -49,7 +62,7 @@ exports.loginUser = asyncHandler(async (req, res) => {
     success: true,
     message: "Login successful.",
     session,
-    user: { fullname: user.fullname, role: user.role },
+    user: { fullname: user?.fullname, role: user?.role },
     accessToken: token,
   });
 });
